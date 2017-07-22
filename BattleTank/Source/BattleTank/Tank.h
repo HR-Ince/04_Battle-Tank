698628497331df98e3c7b519a946e33f5f965825
Unknown <harryrince@gmail.com>
2017-07-22 01:09:23 +0100

    Lecture 135: SuggestProjectileVelocity

--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
@@ -18,15 +19,20 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+    UTankAimingComponent* TankAimingComponent = nullptr;
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
     void AimAt(FVector HitLocation);
+    
+    UFUNCTION(BlueprintCallable, Category = Setup)
+    void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+    
 private:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	
-	
+    
+    UPROPERTY(EditAnywhere, Category = Firing)
+    float LaunchSpeed = 100000;
 };
