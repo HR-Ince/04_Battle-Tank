e01106f842cae2261757e054ff38e3a42c6d93dc
Unknown <harryrince@gmail.com>
2017-09-07 17:26:09 +0100

    Lecture 180: Now with Side Friction

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -2,6 +2,22 @@
 
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    
+    auto SlippageSpeed = FVector::DotProduct(GetComponentVelocity(),GetRightVector());
+    auto CorrectiveAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+    auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+    auto CorrectiveForce = (TankRoot->GetMass() * CorrectiveAcceleration) / 2;
+    
+    TankRoot->AddForce(CorrectiveForce);
+}
 void UTankTrack::SetThrottle(float Throttle)
 {
     auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
