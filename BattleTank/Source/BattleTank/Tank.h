94d8d6294094a8a5150ada9364c3d3fb36fe2f13
Unknown <harryrince@gmail.com>
2017-09-15 18:28:38 +0100

    Lecture 196: Health Bar!

--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -15,6 +15,9 @@ public:
 	// Sets default values for this pawn's properties
 	ATank();
     
+    UFUNCTION(BlueprintPure, Category = "Health")
+    float GetHealthPercent() const;
+    
 private:
     virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
     
